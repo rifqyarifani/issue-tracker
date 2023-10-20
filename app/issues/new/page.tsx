0292c95e8d5aa93e0d24a183d9410d358cc7cb91
rifqyarifani <rifqy.arifani44@gmail.com>
2023-10-20 22:44:49 +0800

    Handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,55 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { TextField } from "@radix-ui/themes";
+import { Callout, TextField } from "@radix-ui/themes";
 import { TextArea } from "@radix-ui/themes";
 import { Button } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+  const [error, setError] = useState("");
   return (
     <>
-      <div className=" max-w-xl space-y-3">
-        <TextField.Root>
-          <TextField.Input placeholder="Title" />
-        </TextField.Root>
-        <SimpleMDE placeholder="Description" />
-        <Button>Submit New Issue</Button>
+      <div className=" max-w-xl">
+        {error && (
+          <Callout.Root color="red" className=" mb-3">
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
+
+        <form
+          className=" space-y-3"
+          onSubmit={handleSubmit(async (data) => {
+            try {
+              await axios.post("/api/issues", data);
+              router.push("/issues");
+            } catch (error) {
+              setError("An unexpected error occured.");
+            }
+          })}
+        >
+          <TextField.Root>
+            <TextField.Input placeholder="Title" {...register("title")} />
+          </TextField.Root>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <SimpleMDE placeholder="Description" {...field} />
+            )}
+          />
+          <Button>Submit New Issue</Button>
+        </form>
       </div>
     </>
   );

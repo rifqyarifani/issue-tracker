b7583fa9edd1575937f27584a23473dc9ba31486
rifqyarifani <rifqy.arifani44@gmail.com>
2023-10-22 11:16:02 +0800

    Show a confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { Issue } from "@prisma/client";
 import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
@@ -10,16 +11,32 @@ interface Props {
 
 const DeleteIssueButton = ({ issue }: Props) => {
   return (
-    <Button color="red">
-      <Link
-        href={`/issues/${issue.id}/delete`}
-        className=" flex items-center gap-1"
-      >
-        {" "}
-        <TrashIcon />
-        Delete Issue
-      </Link>
-    </Button>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action cannot be
+            undone.
+          </AlertDialog.Description>
+          <Flex direction={"row"} mt={"4"} gap={"4"}>
+            <AlertDialog.Cancel>
+              <Button color="gray" variant="soft">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" variant="solid">
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 

b3354d2f63c2511585ac4a9bf0b63af63bcaf3b3
rifqyarifani <rifqy.arifani44@gmail.com>
2023-10-23 20:12:22 +0800

    Refactor : issue  table

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -29,7 +29,7 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
   };
 
   return (
-    <Flex align={"center"} gap={"2"}>
+    <Flex align={"center"} gap={"2"} mt={"4"} justify={"end"}>
       <Text size={"2"}>
         Page {currentPage} of {pageCount}
       </Text>
